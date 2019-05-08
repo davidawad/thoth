6a185f347e44969cd67ce4d9a219cc88579ef228
david awad <me@davidawad.com>
2019-05-08 17:39:26 -0400

    FEAT: cleaning up, trying highlighting as well

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
 ## Speadreader (working title)
 
+This is a speed reading tool for reaching 500wpm or even more.
 
 ## TODO
 
 create class based design for easier implementation
 
-fix error at end of reading in loop function
-
-fix multiple use issues, e.g. playing new segment after reading
-
 space bar to play / pause
 
 ability to set wpm in the UI
 
-smarter hyphenation handling
+smarter hyphenation handling, kinda jank rn
+
+develop unit test suite for js code comparing to the original
 
 
-This is a speed reading tool for reaching 500wpm or even more.
+
+handling for hyphenated acronyms (G-Force, MAPE-K), these are single words, but are rendered separately
+
+center reading box in middle of page
+
+set text box to optimal width for eye travel
+
+add blurb to bottom in light grey "Inspired by Zethos and Spritzer, coded with <3 by David Awad"
+
+display previous words on pause?
+
+open questions :
+
+optimal word length?
+
 
 
 This repo is a fork of [Zethos](http://zethos.zolmeister.com/).
--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// d()
-
-var playing, loop, parse, focus, paused;
+var paused = true
 
 window.index = 0
+
+// speed in words per minute
+let READING_SPEED = 500
+let MAX_WORD_SIZE = 8
 
 // hyphenating
 //    if <= 7 chars
@@ -14,14 +16,23 @@ window.index = 0
 //    else
 //    return {7}, hyphenated{x}
 
-let hyphenate = function(words) {
-    // console.log(words)
-    let ret = words
+let hyphenate = function(word) {
+    let ret = word
+    let len = word.length
 
-    with(words) {
-        ret = length < 8 ? words : length < 11 ? slice(0,length - 3) + '- ' + slice(length-3) : slice(0,7) + '- ' + hyphenate(slice(7))
+    // console.log(word, len)
 
-    }
+    // TODO idfk what this is doing rn
+    ret = len < MAX_WORD_SIZE ? word : len < 11 ? word.slice(0, len - 3) + '- ' + word.slice(len - 3) : word.slice(0,7) + '- ' + hyphenate(word.slice(7))
+
+    // if (len >= 7 && len < 11) {
+        // ret = word.slice(0, len - 3) + '- ' + word.slice(len - 3)
+    // }
+
+    // if (len > 11) {
+        // ret = word.slice(0, 7) + '- ' + hyphenate(word.slice(7))
+    // }
+
 
     return ret
 }
@@ -44,37 +55,49 @@ parse = function(words) {
         .replace(/([.?!])([A-Z-])/g, '$1 $2')
         .split(/\s+/)
         .reduce(function(words, str) {
-            with(str) {
-                // focus point
-                for(let j = focus = (length - 1) / 2 | 0; j >= 0;  j--) {
 
-                    if (/[aeiou]/.test(str[j])) {
-                        focus = j
-                        break;
-                    }
+            let focus
+            let len = str.length
+
+            // focus point is typically going to be
+            // the first vowel in the word
+            for (let j = focus = (len - 1) / 2 | 0; j >= 0;  j--) {
+
+                if (/[aeiou]/.test(str[j])) {
+                    focus = j
+                    break
                 }
-
-                // time on page
-                let t = 60000/500 // 500 wpm
-
-                if (length > 6)
-                    t += t/4
-
-                if (~indexOf(',')){
-                    t += t/2
-                }
-
-                if(/[.?!]/.test(str)) {
-                    t+= t*1.5
-                }
-
-                return length > 14 || length - focus > 7 ? words.concat(parse(hyphenate(str))) : words.concat([[str, focus, t]])
             }
+
+            // time that this word will be displayed
+            let t = 60000 / READING_SPEED
+
+            if (len > 6) {
+                t += t/4
+            }
+
+            if (~str.indexOf(',')) {
+                t += t/2
+            }
+
+            if (/[.?!]/.test(str)) {
+                t += t * 1.5
+            }
+
+            let ret = words.concat([[str, focus, t]])
+
+            if (len > 14 || len - focus > 7 ) {
+                ret = words.concat(parse(hyphenate(str)))
+            }
+
+            return ret
+
         }, [])
 }
 
 
 function play() {
+
     let arr = parse(word_content.textContent)
 
     if (paused) {
@@ -86,40 +109,64 @@ function play() {
 
     // play from beginning
     loop(arr, 0)
+    paused = true
 }
 
 function pause() {
     paused = true
 }
 
+function reset() {
+    pause()
+    window.index = 0
+}
+
+// switch from paused to playing and vice versa
+function playpause() {
+    if (paused) {
+        play()
+    } else {
+        pause()
+    }
+}
 
 // stick the tape in the VHS player ~
-loop = function(arr, index) {
+// take our array and a certain index and display the words in a recursive calling loop.
+function loop(arr, index) {
 
-    console.log(index)
+    // are we at the end of the reading
+    if (index === arr.length){
+        // reset index when done reading
+        window.index = 0
+        return
+    }
 
     if (paused) { return }
 
     // word object
-    var w = arr[index]
+    let w = arr[index]
+    let display_word = w[0]
+    let hot_char_ind = w[1]
+    let timer        = w[2]
 
 
     // 8 here is the string size
-    canvas.innerHTML = Array(8 - w[1]).join('&nbsp;') +
-        w[0].slice(0, w[1]) +
-        '<v>' +
-        w[0][w[1]] + // inject our hot letter
-        '</v>' +
-        w[0].slice(w[1] + 1)
+    let display_HTML = Array(MAX_WORD_SIZE - hot_char_ind).join('&nbsp;') +
+                        display_word.slice(0, hot_char_ind) +
+                        '<v>' +
+                        display_word[hot_char_ind] + // inject our hot letter
+                        '</v>' +
+                        display_word.slice(hot_char_ind + 1)
+
+
+    canvas.innerHTML = display_HTML
 
     // make recursive call
     window.index++
 
-    let next_callback = function(){loop(arr, window.index)}
-    setTimeout(next_callback, w[2])
+    let next_callback = function(){ loop(arr, window.index) }
+    setTimeout(next_callback, timer)
 }
-
-
 
 
 
