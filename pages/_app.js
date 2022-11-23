2089085f18204bfb301d50aaa643272f9b42192f
david awad <me@davidaw.ad>
2022-11-23 01:52:07 -0500

    fix: upgrade to nextjs

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../src/App.css";
+import "../src/components/ModalWrapper/ModalWrapper.css";
+import "../src/components/Reader/Reader.css";
+import "../src/components/FileParser/FileParser.css";
+import "../src/components/SettingsPanel/SettingsPanel.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import App from "../src/App";
+
+function HomePage() {
+  return <App />;
+}
+
+export default HomePage;
