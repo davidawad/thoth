48cde81c1369b225dcaa5565c6d77888fd75b80c
david awad <me@davidaw.ad>
2025-02-19 11:25:47 -0500

    Add Docker testing workflow and Jest config for CSS transform

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import MyApp from "./MyApp";
+import "../styles/globals.css";
+import "../src/App.css";
+import "../src/components/Reader/Reader.css";
+import "../src/components/FileParser/FileParser.css";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<MyApp />);
+  root.unmount();
 });
