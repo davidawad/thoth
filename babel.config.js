54430bd4d6de3bbb105a1585e9f8ba04223ef67b
david awad <me@davidaw.ad>
2025-03-02 16:51:16 +0200

    Update Babel and Next.js configurations

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-react",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic", // This enables the new JSX transform
+      },
+    ],
   ],
 };
